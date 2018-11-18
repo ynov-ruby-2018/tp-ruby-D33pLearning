c2d0d878d239bdb9f143a49ed6c06464af22f867
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:08:13 +0100

    Update routes.rb

--- TP_ruby_tweeter/config/routes.rb
+++ TP_ruby_tweeter/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :homepage, only: [:index, :create]
-  root to: 'homepage#index'
+  resources :homepage, only: [:index, :create] # Bien
+  root to: 'homepage#index' # Vu que c'est la toute première route, préférable de la mettre en premier. Pas de sanction, juste conseil.
 end
