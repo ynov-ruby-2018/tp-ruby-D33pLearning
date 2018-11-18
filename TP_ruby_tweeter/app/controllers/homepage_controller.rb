dd1606f8df9fdf76739b822cb6eca5e56737167d
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:06:18 +0100

    Update homepage_controller.rb

--- TP_ruby_tweeter/app/controllers/homepage_controller.rb
+++ TP_ruby_tweeter/app/controllers/homepage_controller.rb
@@ -3,6 +3,9 @@ class HomepageController < ApplicationController
   def index
      @comments = Comment.all.order('created_at DESC')
     #raise current_user.inspect
+    
+    # Conseil: JAMAIS de code commenté SVP. Nulle part, jamais (valable pour toutes techno)
+    # Ceci dit, ça montre que vous avez la bonne approche sur le debug, ça c'est bien.
   end
 
   def create
@@ -11,6 +14,9 @@ class HomepageController < ApplicationController
       content: params[:comment]
       )
       redirect_to homepage_index_path
+    # Attention au formatage / mise en forme du code SVP !
   end
+  
+  # Aille, il manque des features...
 
 end
