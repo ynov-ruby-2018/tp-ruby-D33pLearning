2c07b88ad5adfc88ce4771b067fbb6c12dd9c2ea
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:09:09 +0100

    Update 20181107155428_create_comments.rb

--- TP_ruby_tweeter/db/migrate/20181107155428_create_comments.rb
+++ TP_ruby_tweeter/db/migrate/20181107155428_create_comments.rb
@@ -2,8 +2,9 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.timestamps
-      t.boolean :liked
+      t.boolean :liked # Boolean ?!
       t.text :content
+      # Manque la clé étrangère pour le user (user_id)
     end
   end
 end
